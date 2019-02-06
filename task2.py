787d594158f13953c7154b1de696b3f7659fd372
Gautam Singh <nonu@TC3FB.WPA.Dal.Ca>
2019-02-06 14:00:28 -0400

    this replace the subnet mask

--- task2.py
+++ task2.py
@@ -3,5 +3,7 @@ with open('running-config.cfg', "w") as file:
 for words in file():
  replace= file.read() . replace('192', 10)
  replace= file.read() . replace('172', 10)
+ replace= file.read() . replace('255.255.0.0', '255.0.0.0')
+ replace= file.read() . replace('255.255.255.0', '255.0.0.0')
 
 file.write(replace)
